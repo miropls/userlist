986d734098b1e8ba54a147058cb7cdfd59c46a68
Miro Paintola <miro.paintola@gmail.com>
2021-04-28 16:04:15 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+      fetch("https://reqres.in/api/users")
+      .then(res => res.json())
+      .then(data => setUsers(data.data))
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          {
+            users.map((user, index) =>
+            <tr key={index}>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.email}</td>
+              <td><img src={user.avatar} alt="kuva"></img></td>
+            </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
